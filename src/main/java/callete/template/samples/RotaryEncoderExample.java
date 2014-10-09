4d5311f68d93ce5f99b7fba7fca1f83eeba18677
syd711 <syd711@googlemail.com>
2014-10-09 23:11:45 +0200

    Initial commit

---
+++ src/main/java/callete/template/samples/RotaryEncoderExample.java
@@ -0,0 +1,34 @@
+package callete.template.samples;
+
+import callete.api.Callete;
+import callete.api.services.gpio.GPIOService;
+import callete.api.services.gpio.RotaryEncoder;
+import callete.api.services.gpio.RotaryEncoderEvent;
+import callete.api.services.gpio.RotaryEncoderListener;
+
+import java.io.IOException;
+
+/**
+ * Sample to show how a rotary encoder works.
+ */
+public class RotaryEncoderExample {
+
+  public static void main(String[] args) {
+    try {
+      GPIOService gpioService = Callete.getGPIOService();
+      RotaryEncoder rotary = gpioService.connectRotaryEncoder(12, 16, "rotary");
+      rotary.addChangeListener(new RotaryEncoderListener() {
+        @Override
+        public void rotated(RotaryEncoderEvent event) {
+          System.out.println("Rotary steps: " + event.getSteps());
+        }
+      });
+
+      System.in.read();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+}
